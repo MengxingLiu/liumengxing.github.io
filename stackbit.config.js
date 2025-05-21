6e63c5d1ad58c5d6640570efdd5541b475e28643
Liu Mengxing <mengluo1990@gmail.com>
2025-05-21 05:12:24 +0200

    Update stackbit.config.js

--- stackbit.config.js
+++ stackbit.config.js
@@ -3,7 +3,7 @@ import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  // ...
+  stackbitVersion: "~0.6.0",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
@@ -11,30 +11,18 @@ export default defineStackbitConfig({
       models: [
         {
           name: "Page",
-          // Define the model as a page model
           type: "page",
           urlPath: "/{slug}",
           filePath: "content/pages/{slug}.json",
           fields: [{ name: "title", type: "string", required: true }]
         }
       ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
+      }
     })
   ]
 });
-
-
-import { ContentfulContentSource } from '@stackbit/cms-contentful'
-
-export default {
-  contentSources: [
-    new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environment: process.env.CONTENTFUL_ENVIRONMENT,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-    }),
-  ],
-  modelExtensions: {
-    // page definitions, field extensions, etc ...
-  },
-}
